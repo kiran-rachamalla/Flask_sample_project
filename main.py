13134d79285a019a997d5f600a1e456606e374b9
kiran-rachamalla <rachamallakiran1@gmail.com>
2025-04-16 20:47:58 +0530

    Changed Saved Message

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def saveQuestions():
     if request.method == "POST":
         question = request.json['a']
         answer = request.json['b']
-        return {"success": "saved sucessfully horray ok so let see "}
+        return {"success": "saved sucessfully horray ok so let"}
 
 
 # app.run(debug=True)
